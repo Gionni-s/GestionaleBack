240ac7d19ce2a520cd82e0c1c61a40d76fd86a2d
Antonio Moscato <moscatoantono@gmail.com>
2024-12-02 10:44:19 +0100

    feat(recipes): add population

--- src/api/recipes/aggregate/searchAggregate.js
+++ src/api/recipes/aggregate/searchAggregate.js
@@ -38,4 +38,73 @@ const searchAggregate = (foodIds) => [
   }
 ];
 
-module.exports = searchAggregate;
+
+const populateAggregate = (userId) => [
+  {
+    $match: {
+      userId: new mongoose.Types.ObjectId(userId)
+    }
+  },
+  {
+    $lookup: {
+      from: 'foods',
+      localField: 'ingridients.foodId',
+      foreignField: '_id',
+      pipeline: [{ $project: { name: 1 } }],
+      as: 'foodDetails'
+    }
+  },
+  {
+    $lookup: {
+      from: 'cookbooks',
+      localField: 'bookId',
+      foreignField: '_id',
+      as: 'bookInfo'
+    }
+  },
+  {
+    $unwind: '$ingridients'
+  },
+  {
+    $addFields: {
+      'ingridients.name': {
+        $arrayElemAt: [
+          {
+            $filter: {
+              input: '$foodDetails',
+              cond: {
+                $eq: [
+                  '$$this._id',
+                  '$ingridients.foodId'
+                ]
+              }
+            }
+          },
+          0
+        ]
+      }
+    }
+  },
+  {
+    $set: {
+      'ingridients.name': '$ingridients.name.name'
+    }
+  },
+  {
+    $unwind: '$bookInfo'
+  },
+  {
+    $group: {
+      _id: '$_id',
+      ingridients: { $push: '$ingridients' },
+      name: { $first: '$name' },
+      userId: { $first: '$userId' },
+      bookId: { $first: '$bookId' },
+      bookInfo: { $first: '$bookInfo' },
+      __v: { $first: '$__v' }
+    }
+  }
+];
+
+
+module.exports = { searchAggregate, populateAggregate };
